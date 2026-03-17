cb652630935bf1de978a0578862fef1c9c560fe8
businessudane-spec <business.udane@gmail.com>
2026-03-17 14:07:00 +0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
 const menuItems = [
   { name: "Home", link: "/" },
   { name: "About", link: "/about" },
-  { name: "Works", link: "/#works" }, // 👈 ADD THIS
+  { name: "Works", link: "/works" }, 
 ];
 
   return (
