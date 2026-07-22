e490b9b87195098dad2177f86915d559591aee5f
businessudane-spec <business.udane@gmail.com>
2026-07-22 21:07:30 +0400

    feat: add Google Tag Manager script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -23,6 +24,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cn("font-sans", geist.variable)}>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-R3S895T3XF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-R3S895T3XF');
+          `}
+        </Script>
+      </head>
       <body className={`${poppins.className} bg-black text-white`}>
         <Header />
         {children}
